2f62815ffbf8c7d1daddeee499e1acdb8cb0df35
21kkfy <21kkfy@gmail.com>
2022-08-18 21:38:58 +0300

    Update current_weather.dart

--- lib/views/current_weather.dart
+++ lib/views/current_weather.dart
@@ -10,7 +10,19 @@ Widget currentWeather(IconData icon, String temp, String location) {
           icon,
           color: Colors.orangeAccent,
           size: 64.0,
-        )
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          "$temp",
+          style: const TextStyle(fontSize: 46.0),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "$location",
+          style: const TextStyle(fontSize: 18.0, color: Color(0xFF5a5a5a)),
+        ),
       ],
     ),
   );
