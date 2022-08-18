20e22eefc45dca486e30e159269e75829a8098e1
21kkfy <21kkfy@gmail.com>
2022-08-18 21:14:41 +0300

    Displaying the weather on a location widget.

---
+++ lib/views/current_weather.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Widget currentWeather(IconData icon, String temp, String location) {
+  return Center(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: Colors.orangeAccent,
+          size: 64.0,
+        )
+      ],
+    ),
+  );
+}
