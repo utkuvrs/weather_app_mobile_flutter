e2acb42ff8111e24bc1fa493dd646652d274ba95
21kkfy <21kkfy@gmail.com>
2022-08-18 22:06:58 +0300

    Fixed columns, requires API integration

--- lib/views/additional_information.dart
+++ lib/views/additional_information.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 Widget additionalInformation(
     String wind, String humidity, String pressure, String feelsLike) {
+  TextStyle titleFont =
+      const TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0);
+  TextStyle infoFont =
+      const TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0);
   return Container(
     width: double.infinity,
     padding: const EdgeInsets.all(10.0),
@@ -14,15 +18,73 @@ Widget additionalInformation(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Text(
                   "Wind",
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0),
+                  style: infoFont,
+                ),
+                const SizedBox(
+                  height: 18.0,
+                ),
+                Text(
+                  "Pressure",
+                  style: infoFont,
                 )
               ],
-            )
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$wind",
+                  style: infoFont,
+                ),
+                const SizedBox(
+                  height: 18.0,
+                ),
+                Text(
+                  "$pressure",
+                  style: infoFont,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Humidity",
+                  style: infoFont,
+                ),
+                const SizedBox(
+                  height: 18.0,
+                ),
+                Text(
+                  "Feels like",
+                  style: infoFont,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$humidity",
+                  style: infoFont,
+                ),
+                const SizedBox(
+                  height: 18.0,
+                ),
+                Text(
+                  "$feelsLike",
+                  style: infoFont,
+                )
+              ],
+            ),
           ],
         )
       ],
