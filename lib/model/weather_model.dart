f35161f40e1121283121728d72dc5c9fd61725f6
21kkfy <21kkfy@gmail.com>
2022-08-19 10:43:48 +0300

    Creating model for Weather objects
    With the help of this object we are going to parse the JSON data to these objects.

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,28 @@
+class Weather {
+  String? cityName;
+  double? temp;
+  double? wind;
+  int? humidity;
+  int? pressure;
+  double? feelsLike;
+
+  Weather(
+      {this.cityName,
+      this.temp,
+      this.wind,
+      this.humidity,
+      this.feelsLike,
+      this.pressure});
+
+  // We are going to parse JSON data to weather models,
+  // and after that we'll use it on the backend.
+
+  Weather.fromJson(Map<String, dynamic> json) {
+    cityName = json["name"];
+    temp = json["main"]["temp"];
+    wind = json["main"]["speed"];
+    humidity = json["main"]["humidity"];
+    feelsLike = json["main"]["feels_like"];
+    pressure = json["main"]["pressure"];
+  }
+}
