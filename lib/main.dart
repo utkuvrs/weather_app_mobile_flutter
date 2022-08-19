f8d5b243299b5ed0029fb18f2db866aa322e27aa
21kkfy <21kkfy@gmail.com>
2022-08-19 19:11:55 +0300

    LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: HomePage(),
+    home: LoginPage(),
   ));
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {}
 }
