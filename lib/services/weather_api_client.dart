939d931df95da19495e9fa5fc59891f95575ada3
21kkfy <21kkfy@gmail.com>
2022-08-19 11:32:16 +0300

    Adjusted API values to metric

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -6,10 +6,10 @@ import 'package:weather_app_mobile_flutter/model/weather_model.dart';
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
     var endPoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=9831b85e6410d2ae724f4f85ddbbde84");
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=9831b85e6410d2ae724f4f85ddbbde84&units=metric");
     var response = await http.get(endPoint);
     var body = jsonDecode(response.body);
-    print(Weather.fromJson(body));
+    print(Weather.fromJson(body).cityName);
     return Weather.fromJson(body);
   }
 }
