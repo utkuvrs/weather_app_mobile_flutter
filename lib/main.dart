fc8ba198941a396d23558f6ddc1e96bfcf6e6ee3
21kkfy <21kkfy@gmail.com>
2022-08-19 12:36:55 +0300

    Display the model attributes
    Display the model values that we got from the API call.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app_mobile_flutter/services/weather_api_client.dart';
 import 'package:weather_app_mobile_flutter/views/additional_information.dart';
 import 'package:weather_app_mobile_flutter/views/current_weather.dart';
+
+import 'model/weather_model.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -16,49 +19,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  WeatherApiClient client = WeatherApiClient();
+  Weather data = Weather();
+  @override
+  void initState() {
+    super.initState();
+    client.getCurrentWeather("Georgia");
+  }
+
+  Future<void> getData() async {
+    data = (await client.getCurrentWeather("Samsun"))!;
+  }
+
   @override
   Widget build(BuildContext context) {
     // Starting with the UI of the app.
     return Scaffold(
-      backgroundColor: const Color(0xFFf9f9f9),
-      appBar: AppBar(
-        backgroundColor: const Color(0XFFf9f9f9),
-        elevation: 0.0,
-        title: const Text(
-          "Weather App",
-          style: TextStyle(color: Colors.black),
+        backgroundColor: const Color(0xFFf9f9f9),
+        appBar: AppBar(
+          backgroundColor: const Color(0XFFf9f9f9),
+          elevation: 0.0,
+          title: const Text(
+            "Weather App",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+            color: Colors.black,
+          ),
         ),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.menu),
-          color: Colors.black,
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          currentWeather(Icons.wb_sunny_rounded, "26.3", "Georgia"),
-          const SizedBox(
-            height: 60.0,
-          ),
-          const Text(
-            "Additional Information",
-            style: TextStyle(
-                fontSize: 24.0,
-                color: Color(0xdd212121),
-                fontWeight: FontWeight.bold),
-          ),
-          const Divider(),
-          const SizedBox(
-            height: 20.0,
-          ),
-          // Adding additional information
-          additionalInformation("24", "2", "1014", "24.0"),
-          // UI done.
-          // Integrate API
-        ],
-      ),
-    );
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // When the connection is over,
+              // display the necessary data.
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Let's create custom widgets for the app.
+
+                  currentWeather(Icons.wb_sunny_rounded, "${data.temp}",
+                      "${data.cityName}"),
+                  const SizedBox(height: 60),
+                  const Text(
+                    "Additional Information",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        color: Color(0xdd212121),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Divider(),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+
+                  additionalInformation("${data.wind}", "${data.humidity}",
+                      "${data.pressure}", "${data.feelsLike}"),
+                ],
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Container();
+          },
+        ));
   }
 }
