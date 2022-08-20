d6dde15489e577368cafcd86d3f78b50b7768eaf
21kkfy <21kkfy@gmail.com>
2022-08-20 11:41:00 +0300

    API key is now encapsulated

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:weather_app_mobile_flutter/model/weather_model.dart';
 
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
+    await dotenv.load(fileName: ".env");
+    String? apiKey = dotenv.env['API_KEY'];
     var endPoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=9831b85e6410d2ae724f4f85ddbbde84&units=metric");
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric");
     var response = await http.get(endPoint);
     var body = jsonDecode(response.body);
     print(Weather.fromJson(body).cityName);
@@ -15,8 +18,10 @@ class WeatherApiClient {
 
   Future<Weather>? getCurrentWeatherFromLatAndLon(
       double lat, double lon) async {
+    await dotenv.load(fileName: ".env");
+    String? apiKey = dotenv.env['API_KEY'];
     var endPoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=9831b85e6410d2ae724f4f85ddbbde84&units=metric");
+        "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric");
 
     var response = await http.get(endPoint);
     var body = jsonDecode(response.body);
