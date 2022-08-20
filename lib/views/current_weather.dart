f54f86ab0d32e7ccc38a1b1d7d6aa2233ed572a6
21kkfy <21kkfy@gmail.com>
2022-08-20 11:55:01 +0300

    Now displaying cloudy & rounded

--- lib/views/current_weather.dart
+++ lib/views/current_weather.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-Widget currentWeather(IconData icon, String temp, String location) {
+Widget currentWeather(String temp, String location) {
+  if (double.parse(temp) > 25.0) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.wb_sunny_rounded,
+            color: Colors.orangeAccent,
+            size: 64.0,
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            "$temp",
+            style: const TextStyle(fontSize: 46.0),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            "$location",
+            style: const TextStyle(fontSize: 18.0, color: Color(0xFF5a5a5a)),
+          ),
+        ],
+      ),
+    );
+  }
   return Center(
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(
-          icon,
+        const Icon(
+          Icons.wb_cloudy_rounded,
           color: Colors.orangeAccent,
           size: 64.0,
         ),
