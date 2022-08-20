5ac038740a6ee5a23890a622f6b3def882cc6bba
21kkfy <21kkfy@gmail.com>
2022-08-20 10:30:09 +0300

    try catch

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -64,8 +64,12 @@ class _LoginWidgetState extends State<LoginWidget> {
   }
 
   Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim());
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+    } catch (e) {
+      print(e);
+    }
   }
 }
