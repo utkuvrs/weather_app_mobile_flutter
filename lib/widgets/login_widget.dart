99bbb0481289e466d4c72c91034ece178839aaa0
21kkfy <21kkfy@gmail.com>
2022-08-20 10:29:59 +0300

    Wrapped into a MaterialApp

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -21,39 +21,44 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          TextField(
-            controller: emailController,
-            cursorColor: Colors.white,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(labelText: "E-mail"),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          TextField(
-            controller: passwordController,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(labelText: "Password"),
-            obscureText: true,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton.icon(
-              onPressed: signIn,
-              icon: const Icon(Icons.lock_open),
-              label: const Text("Sign In", style: TextStyle(fontSize: 24)),
-              style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50))),
-        ],
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      title: "Weather App",
+      home: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            TextField(
+              controller: emailController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "E-mail"),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+                onPressed: signIn,
+                icon: const Icon(Icons.lock_open),
+                label: const Text("Sign In", style: TextStyle(fontSize: 24)),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50))),
+          ],
+        ),
       ),
     );
   }
